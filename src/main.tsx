2e59a1691bfe332c093ef4eb0bb4c0ccdb3c6518
Lucas Cezar <lmcezar@gmail.com>
2022-07-03 22:54:30 -0300

    Roteamento e player da aula

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
-import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { client } from './lib/apollo'
 import './styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
